0de84c077fd37abe70d59e480f0dfa0eb783389f
pkr187 <pankajroman187@gmail.com>
2023-02-13 12:06:48 +0530

    [pankaj] Add:Multithreading

---
+++ Multithreading/EmployeeDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    internal class EmployeeDetails
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Phoneno { get; set; }
+        public string Department { get; set; }
+
+        public EmployeeDetails(int employeeID, string employeeName, string phoneno, string department)
+        {
+            EmployeeID = employeeID;
+            EmployeeName = employeeName;
+            Phoneno = phoneno;
+            Department = department;
+        }
+    }
+}
---
+++ Multithreading/EmployeePayRollOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    internal class EmployeePayRollOperation
+    {
+        List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
+        public void AddEmployeeToPayroll(List<EmployeeDetails> listemp)
+        {
+            listemp.ForEach(empdata =>
+            {
+                Console.WriteLine("employee being added:" + empdata.EmployeeName);
+                this.AddEmployeeToPayroll(empdata);
+                Console.WriteLine("employee added:" + empdata.EmployeeName);
+            });
+
+        }
+        private void AddEmployeeToPayroll(EmployeeDetails emp)
+        {
+            employeeDetails.Add(emp);
+        }
+    }
+}
